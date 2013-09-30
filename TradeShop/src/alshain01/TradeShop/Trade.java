ed2f379146b264486833f3ac2d3f56b631d4e2d4
Kevin Seiden <Kevin Seiden@KevinSeiden-PC>
2013-09-30 15:04:27 -0500

    Update for multiple buy items

--- TradeShop/src/alshain01/TradeShop/Trade.java
+++ TradeShop/src/alshain01/TradeShop/Trade.java
@@ -5,37 +5,70 @@ import java.util.Date;
 import org.bukkit.Material;
 
 public class Trade {
+	private Long id;
 	private long createTime;
-	private Material tradeItem;
-	private Material tradeForItem;
-	private int tradeQty;
-	private int tradeForQty;
+	private TradeItem[] items = new TradeItem[3];
 	
-	public Trade(Material tradeItem, Material tradeForItem, int tradeQty, int tradeForQty) {
-		this.tradeItem = tradeItem;
-		this.tradeForItem = tradeForItem;
-		this.tradeQty = tradeQty;
-		this.tradeForQty = tradeForQty;
+	public class TradeItem {
+		private Material item;
+		private int quantity;
+		
+		public int getQuantity() {
+			return quantity;
+		}
+		
+		public Material getItem() {
+			return item;
+		}
+	}
+	
+	public Trade(Material sellItem, Material buyItem, int sellQty, int buyQty) {
+		id = null;
+		
+		items[0].item  = buyItem;
+		items[1].item  = null;
+		items[2].item = sellItem;
+		
+		items[0].quantity = buyQty;
+		items[1].quantity = 0;
+		items[2].quantity = sellQty;
+
+		
 		this.createTime = new Date().getTime();
 	}
 	
-	public Material getTradeItem() {
-		return tradeItem;
+	public Trade(Material sellItem, Material buyItem1, Material buyItem2, int sellQty, int buyQty1, int buyQty2) {
+		id = null;
+		
+		items[0].item  = buyItem1;
+		items[1].item  = buyItem2;
+		items[2].item = sellItem;
+		
+		items[0].quantity = buyQty1;
+		items[1].quantity = buyQty2;
+		items[2].quantity = sellQty;
+
+		
+		this.createTime = new Date().getTime();
 	}
 	
-	public Material getTradeForItem() {
-		return tradeForItem;
+	public TradeItem getSellItem() {
+		return items[2];
 	}
 	
-	public int getTradeQuantity() {
-		return tradeQty;
+	public TradeItem getBuyItem1() {
+		return items[0];
 	}
 	
-	public int getTradeForQuantity() {
-		return tradeForQty;
+	public TradeItem getBuyItem2() {
+		return items[1];
 	}
 	
 	public long getCreationTime() {
 		return createTime;
+	}
+	
+	public long getID() {
+		return id;
 	}
 }
